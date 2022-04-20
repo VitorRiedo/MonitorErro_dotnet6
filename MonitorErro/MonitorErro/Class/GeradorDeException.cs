2c837b6aa452da691cda99af773873cd4b163281
VitorRiedo <vitorriedo@gmail.com>
2022-04-19 21:58:35 -0300

    Gerando Excepition

---
+++ MonitorErro/MonitorErro/Class/GeradorDeException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorErro.Class
+{
+    enum tpException
+    {
+        ArgumentException,
+        ArgumentNullException,
+        ArgumentOutOfRangeException,
+        DivideByZeroException,
+    }
+
+    public class GeradorDeException
+    {
+        Random random = new Random();
+
+        public GeradorDeException()
+        {
+        }
+
+        public void GetException()
+        {
+            switch (random.Next(Enum.GetNames(typeof(tpException)).Length))
+            {
+                case (int)tpException.ArgumentException:
+                    throw new ArgumentException();
+                    break;
+                case (int)tpException.ArgumentNullException:
+                    throw new ArgumentNullException();
+                    break;
+                case (int)tpException.ArgumentOutOfRangeException:
+                    throw new ArgumentOutOfRangeException();
+                    break;
+                case (int)tpException.DivideByZeroException:
+                    throw new DivideByZeroException();
+                    break;
+                default:
+                    throw new Exception();
+                    break;
+            }
+        }
+
+    }
+}
--- MonitorErro/MonitorErro/Program.cs
+++ MonitorErro/MonitorErro/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MonitorErro.Class;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var geradorDeExcepition = new GeradorDeException();
+
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                geradorDeExcepition.GetException();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Gerou o Erro: {e.GetType().ToString().Replace("System.","").PadRight(30)} - {e.Message}");
+            }
+        }
+
+        Console.WriteLine($"\n\nFinalizou\n\n");
+    }
+}
